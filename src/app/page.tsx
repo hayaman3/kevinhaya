a331c96e62a13b88d19c11b428ca359c4bc525fe
hayaman3 <kevin.haya@yahoo.com>
2023-08-10 21:18:20 +0800

    Added tailwind for sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Header from "./header/Header";
+import classNames from "classnames";
 
 export default function Home() {
+  const sectionClass = classNames(
+    "pt-24",
+    "pb-8",
+    "px-0",
+    "sm:pt-8",
+    "sm:pb-16",
+    "sm:px-0",
+  );
+
   return (
     <>
       <Header />
       <main>
-        {/* <Sections class/> */}
+        {/* <Home className={sectionClass}/> */}
         <div className="min-h-[200vh] bg-zinc-100">asd</div>
       </main>
     </>
