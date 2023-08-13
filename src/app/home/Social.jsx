f262da469aa743336b102f916f872bc68140ab4d
hayaman3 <kevin.haya@yahoo.com>
2023-08-13 18:18:32 +0800

    bare Home component added

--- src/app/home/Social.jsx
+++ src/app/home/Social.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function Social() {
-	return (
-		<div className="home-social">
-			<a
-				href="https://github.com/hayaman3"
-				className="home-social-icon"
-				target="_blank"
-				rel="noopener noreferrer">
-				<i className="uil uil-github" />
-			</a>
-			<a
-				href="https://www.linkedin.com/in/kevin-larry-haya-458958198/"
-				className="home-social-icon"
-				target="_blank"
-				rel="noopener noreferrer">
-				<i className="uil uil-linkedin" />
-			</a>
+  return (
+    <div className="home-social">
+      <a
+        href="https://github.com/hayaman3"
+        className="home-social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="uil uil-github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/kevin-larry-haya-458958198/"
+        className="home-social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="uil uil-linkedin" />
+      </a>
 
-			<a
-				href="https://www.facebook.com/kevinlarry.haya/"
-				className="home-social-icon"
-				target="_blank"
-				rel="noopener noreferrer">
-				<i className="uil uil-facebook" />
-			</a>
-		</div>
-	);
+      <a
+        href="https://www.facebook.com/kevinlarry.haya/"
+        className="home-social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="uil uil-facebook" />
+      </a>
+    </div>
+  );
 }
 
 export default Social;
