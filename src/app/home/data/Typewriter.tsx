160cb6ce278df94d335c878eb20f1764cdf6ce83
hayaman3 <kevin.haya@yahoo.com>
2023-08-24 16:25:40 +0800

    remove unnecessary code

--- src/app/home/data/Typewriter.tsx
+++ src/app/home/data/Typewriter.tsx
@@ -1,43 +1,4 @@
 "use client";
-// import React, { FunctionComponent, useState, useEffect } from "react";
-
-// interface TypewriterProps {
-//   text: string;
-//   delay: number;
-//   infinite?: boolean;
-// }
-
-// const Typewriter: FunctionComponent<TypewriterProps> = ({
-//   text,
-//   delay,
-//   infinite = false,
-// }) => {
-//   const [currentText, setCurrentText] = useState<string>("");
-//   const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-//   useEffect(() => {
-//     let timeout: NodeJS.Timeout;
-
-//     if (currentIndex < text.length) {
-//       timeout = setTimeout(() => {
-//         setCurrentText((prevText) => prevText + text[currentIndex]);
-//         setCurrentIndex((prevIndex) => prevIndex + 1);
-//       }, delay);
-//     } else if (infinite) {
-//       setCurrentIndex(0);
-//       setCurrentText("");
-//     }
-
-//     return () => clearTimeout(timeout);
-//   }, [currentIndex, delay, infinite, text]);
-
-//   return (
-//     <h3 className="relative mb-4 text-xl font-normal text-title md:text-base">
-//       {currentText}
-//     </h3>
-//   );
-// };
-
 import React, { useState, useEffect } from "react";
 
 interface TypewriterProps {
