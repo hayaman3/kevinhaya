06109fdde270704e366cffc23d75488242d5b145
hayaman3 <kevin.haya@yahoo.com>
2023-08-28 22:38:33 +0800

    changed alt attribute

--- src/app/home/Image.tsx
+++ src/app/home/Image.tsx
@@ -6,7 +6,7 @@ const ProfileImage: FunctionComponent = () => {
     <div className="sm:justify-self initial relative order-1 h-[300px] w-[300px] animate-border justify-self-center overflow-hidden rounded-[60%_40%_30%_70%/60%_30%_70%_40%] shadow-[inset_0_0_0_9px_rgb(255_255_255/_30%)] md:h-[250px] md:w-[250px] md:shadow-[inset_0_0_0_8px_rgb(255_255_255/_30%)] sm:order-[initial] sm:h-[200px] sm:w-[200px] sm:shadow-[inset_0_0_0_6px_rgb(255_255_255/_30%)]">
       <Image
         src="/./images/kevin.png"
-        alt="logo"
+        alt="cover image"
         priority
         fill
         sizes="(max-width: 768px) 200px, (max-width: 992px) 250px, 300px"
