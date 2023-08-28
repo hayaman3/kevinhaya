2a24df58d9aab8ee69d2d643c2e71acb6f6c6045
hayaman3 <kevin.haya@yahoo.com>
2023-08-28 22:36:57 +0800

    removed unused code on Image.tsx

--- src/app/about/Image.tsx
+++ src/app/about/Image.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
 import Image from "next/image";
 
+const containerStyle: string = "";
+
 const ProfileImage: FunctionComponent = () => {
   return (
     <div className="sm:justify-self initial relative order-1 h-[300px] w-[300px] animate-border justify-self-center overflow-hidden rounded-[60%_40%_30%_70%/60%_30%_70%_40%] shadow-[inset_0_0_0_9px_rgb(255_255_255/_30%)] md:h-[250px] md:w-[250px] md:shadow-[inset_0_0_0_8px_rgb(255_255_255/_30%)] sm:order-[initial] sm:h-[200px] sm:w-[200px] sm:shadow-[inset_0_0_0_6px_rgb(255_255_255/_30%)]">
