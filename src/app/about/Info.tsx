161ed8653446e7aa83281468f75602e9032b1207
hayaman3 <kevin.haya@yahoo.com>
2023-08-30 20:16:32 +0800

    changed Icon type from ComponentType to IconType to pass props with no errors

--- src/app/about/Info.tsx
+++ src/app/about/Info.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { BiIdCard, BiMap, BiCodeAlt } from "react-icons/bi";
+import { IconType } from "react-icons";
 
 const infoList: Array<{
-  Icon: React.ComponentType;
+  Icon: IconType;
   title: string;
   subtitle: string;
 }> = Array(
@@ -27,9 +28,10 @@ const Info: FunctionComponent = () => {
   return (
     <div className="mb-8 grid grid-cols-[repeat(3,140px)] gap-2 md:justify-center ss:grid-cols-[repeat(3,1fr)] xs:grid-cols-[repeat(2,1fr)]">
       {infoList.map((info) => {
+        const Icon = info.Icon;
         return (
-          <div className="flex flex-col items-center rounded-xl border border-solid border-[#0000001a] bg-white px-5 py-4 text-center md:px-2 md:py-3">
-            <info.Icon />
+          <div className="flex flex-col items-center rounded-xl border border-solid border-[#0000001a] bg-white px-2 py-3 text-center">
+            <Icon className="text-tile mb-2 text-2xl" />
             <h3 className="text-sm font-medium text-title">{info.title}</h3>
             <span className="text-[0.625rem]">{info.subtitle}</span>
           </div>
