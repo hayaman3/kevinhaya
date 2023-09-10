ad98833aad37720bcec35045e007217653ae19e3
hayaman3 <kevin.haya@yahoo.com>
2023-09-10 19:51:59 +0800

    fixed tailwind ui bug on Form.tsx

--- src/app/contact/Form.tsx
+++ src/app/contact/Form.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { FunctionComponent } from "react";
+import Button from "./form/Button";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
@@ -34,52 +35,64 @@ const ContactForm: FunctionComponent = () => {
   });
 
   return (
-    <div className="min-h-[11rem]">
+    <div>
       <h3 className="mb-6 text-center text-xl font-medium md:text-base">
         Write to me
       </h3>
       <form className="flex w-[360px] flex-col items-center justify-center gap-8 sm:mx-auto sm:my-0 ss:w-full">
-        <div className=" relative h-16 w-full">
-          <label htmlFor="name" className="contact-form-tag">
+        <div className="relative h-16 w-full">
+          <label
+            htmlFor="name"
+            className="absolute left-5 top-[-1rem] z-10 bg-zinc-50 p-1 text-[0.8rem] md:text-[0.75rem]"
+          >
             Name
           </label>
           <input
             type="text"
             name="name"
-            className="contact-form-input"
-            placeholder="Insert your name"
+            className="text-[hsl(0,0%,46%) absolute left-0 top-0 z-[1] h-full w-full rounded-xl border-2 border-solid border-[rgba(0,0,0,0.3)] bg-zinc-50 p-6"
+            placeholder="Enter your name"
           />
         </div>
 
         <div className="contact-form-div">
-          <label htmlFor="email" className="contact-form-tag">
+          <label
+            htmlFor="email"
+            className="absolute left-5 top-[-1rem] z-10 bg-zinc-50 p-1 text-[0.8rem] md:text-[0.75rem]"
+          >
             Mail
           </label>
           <input
             type="email"
             name="email"
-            className="contact-form-input"
-            placeholder="Insert your email"
+            className="text-[hsl(0,0%,46%) absolute left-0 top-0 z-[1] h-full w-full rounded-xl border-2 border-solid border-[rgba(0,0,0,0.3)] bg-zinc-50 p-6"
+            placeholder="Enter your email"
           />
         </div>
 
-        <div className="contact-form-div contact-form-area">
-          <label htmlFor="mesage" className="contact-form-tag">
+        <div className="relative h-16 min-h-[11rem] w-full">
+          <label
+            htmlFor="mesage"
+            className="absolute left-5 top-[-1rem] z-10 bg-zinc-50 p-1 text-[0.8rem] md:text-[0.75rem]"
+          >
             Message
           </label>
           <textarea
             name="message"
             cols={30}
             rows={10}
-            className="contact-form-input resize-none"
-            placeholder="Your message"
+            className="text-[hsl(0,0%,46%) absolute left-0 top-0 z-[1] h-full w-full resize-none rounded-xl border-2 border-solid border-[rgba(0,0,0,0.3)] bg-zinc-50 p-6"
+            placeholder="Enter your message"
           />
         </div>
-        <button type="submit" className="button button-flex">
+        <button
+          type="submit"
+          className="inline-flex items-center rounded-2xl bg-title px-8 py-5 font-medium text-white hover:bg-black md:px-7 md:py-4"
+        >
           Send Message
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="button-icon"
+            className="ml-2 md:h-[22px] md:w-[22px]"
             width="24"
             height="24"
             viewBox="0 0 24 24"
@@ -87,14 +100,15 @@ const ContactForm: FunctionComponent = () => {
           >
             <path
               d="M14.2199 21.9352C13.0399 21.9352 11.3699 21.1052 10.0499 17.1352L9.32988 14.9752L7.16988 14.2552C3.20988 12.9352 2.37988 11.2652 2.37988 10.0852C2.37988 8.91525 3.20988 7.23525 7.16988 5.90525L15.6599 3.07525C17.7799 2.36525 19.5499 2.57525 20.6399 3.65525C21.7299 4.73525 21.9399 6.51525 21.2299 8.63525L18.3999 17.1252C17.0699 21.1052 15.3999 21.9352 14.2199 21.9352ZM7.63988 7.33525C4.85988 8.26525 3.86988 9.36525 3.86988 10.0852C3.86988 10.8052 4.85988 11.9052 7.63988 12.8252L10.1599 13.6652C10.3799 13.7352 10.5599 13.9152 10.6299 14.1352L11.4699 16.6552C12.3899 19.4352 13.4999 20.4252 14.2199 20.4252C14.9399 20.4252 16.0399 19.4352 16.9699 16.6552L19.7999 8.16525C20.3099 6.62525 20.2199 5.36525 19.5699 4.71525C18.9199 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
-              fill="var(--container-color)"
+              fill="#ffffff"
             />
             <path
               d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z"
-              fill="var(--container-color)"
+              fill="#ffffff"
             />
           </svg>
         </button>
+        {/* <Button /> */}
       </form>
     </div>
     // <div>
