b4cefa44faa234d08f8badf6db9b6302d6ce19ee
hayaman3 <kevin.haya@yahoo.com>
2023-09-26 20:12:00 +0800

    added nodemailer to Form component

--- src/app/contact/Form.tsx
+++ src/app/contact/Form.tsx
@@ -6,8 +6,37 @@ import Textarea from "./form/Textarea";
 import Button from "./form/Button";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import transporter from "./nodemailer";
+// import { sendContactForm } from "../lib/api";
+
+// const handleSubmit
 
 const ContactForm: FunctionComponent = () => {
+  const handleSubmit = async (
+    values: any,
+    { setSubmitting, resetForm }: any,
+  ) => {
+    try {
+      await transporter.sendMail({
+        from: "your-email@gmail.com",
+        to: "recipient@example.com",
+        subject: "New Contact Form Submission",
+        html: `
+          <p>Name: ${values.name}</p>
+          <p>Email: ${values.email}</p>
+          <p>Message: ${values.message}</p>
+        `,
+      });
+
+      // Email sent successfully
+      resetForm();
+      setSubmitting(false);
+    } catch (error) {
+      console.error("Error sending email:", error);
+      // Handle error, show a message to the user, etc.
+    }
+  };
+
   return (
     <div>
       <h3 className="mb-6 text-center text-xl font-medium md:text-base">
@@ -27,12 +56,19 @@ const ContactForm: FunctionComponent = () => {
             .required("Email is required"),
           message: Yup.string().required("Message is required"),
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
+        // onSubmit={async (values, { setSubmitting }) => {
+        //   // setTimeout(() => {
+        //   //   // alert(JSON.stringify(values, null, 2));
+
+        //   // }, 400);
+        //   await sendContactForm
+        //   setSubmitting(false);
+        // }}
+        // onSubmit={async (values) => {
+        //   await sendContactForm(values);
+        //   // alert(JSON.stringify(values, null, 2));
+        // }}
+        onSubmit={handleSubmit}
       >
         <Form className="flex w-[360px] flex-col items-center justify-center gap-8 sm:mx-auto sm:my-0 ss:w-full">
           <InputRow>
