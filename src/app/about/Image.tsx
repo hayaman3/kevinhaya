5d9207a429f1ce6db5bf7bff5fdddb316eaa7a6c
hayaman3 <kevin.haya@yahoo.com>
2023-09-29 19:19:09 +0800

    added new image for about section

--- src/app/about/Image.tsx
+++ src/app/about/Image.tsx
@@ -3,15 +3,14 @@ import Image from "next/image";
 
 const AboutImage: FunctionComponent = () => {
   return (
-    <div className="relative h-[300px] w-[300px] justify-self-center overflow-hidden rounded-3xl md:h-[220px]  md:w-[220px]">
-      <Image
-        src="/./images/kevin.png"
-        alt="about image"
-        fill
-        sizes="(max-width: 992px) 220px, 300px"
-        className="-z-10 object-cover grayscale-[60%]"
-      />
-    </div>
+    <Image
+      alt="kevin graduation loopsie"
+      src="/./images/kevin-graduation-loopsie.jpg"
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="h-auto w-[300px]  rounded-xl md:w-[220px]"
+    />
   );
 };
 
